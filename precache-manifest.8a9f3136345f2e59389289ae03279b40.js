350617324298260e42897e9a8fc22aeab1a69a9e
Shadout <miguelyyemma@gmail.com>
2022-03-21 13:52:01 +0100

    Updates

---
+++ precache-manifest.8a9f3136345f2e59389289ae03279b40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef91f4ad2ecd3dd44ff0fd176af11d5",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "648498505e3d48e32397",
+    "url": "/repository-name/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "a804a9968cbd967f374d",
+    "url": "/repository-name/static/js/2.d6d2c6b6.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/repository-name/static/js/2.d6d2c6b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "648498505e3d48e32397",
+    "url": "/repository-name/static/js/main.e4596b4b.chunk.js"
+  },
+  {
+    "revision": "e4af8a347144472c14df",
+    "url": "/repository-name/static/js/runtime-main.c1280fbe.js"
+  }
+]);
